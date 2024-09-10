25d9e00972845b89f567942574fa51cf31e59865
manoj chaudhary <manoj.raju-chaudhary@capgemini.com>
2024-09-10 11:15:44 +0530

    added usecase 1

--- APIGWLMDAS3/infra/lib/infra-stack.ts
+++ APIGWLMDAS3/infra/lib/infra-stack.ts
@@ -1,8 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3'
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as iam from 'aws-cdk-lib/aws-iam'
+import { Lambda } from 'aws-cdk-lib/aws-ses-actions';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import assert = require('assert');
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -13,9 +15,23 @@ export class InfraStack extends cdk.Stack {
   const mys3bucket = new s3.Bucket(this, 'mydemobuckets3',{
        bucketName : 'demobuckettoday0809',
        versioned: true
-
   })
  
+  // create IAM role
+  const lamdarole = new iam.Role(this,' myrole' ,{
+    roleName: 'lambdaRole',
+    description: 'The role is to access the s3 bucket',
+    assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+  }) 
+  lamdarole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmzonS3FullAccess'));
+
+  const bankingLambda = new lambda.Function(this, 'bankingLambda',{
+   code: lambda.Code.fromAsset('../app'),
+   handler:"lambda_function.lambda_handler",
+   runtime: lambda.Runtime.PYTHON_3_8,  //should declare first to avoid onscreen error
+   role: lamdarole
+
+  })
    
   }
 }
